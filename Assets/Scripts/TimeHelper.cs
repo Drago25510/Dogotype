3e8bc6e95f128b6197cb1ef27632b197707fc316
Drago25510 <38044491+Drago25510@users.noreply.github.com>
2018-04-03 18:18:11 +0300

    Initial commit

---
+++ Assets/Scripts/TimeHelper.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeHelper : MonoBehaviour
+{
+    public int MaxHealth = 100;
+    public int Health = 100;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+   public void GetTap(int damage)
+    {
+        int health = Health - damage;
+     
+               if (health <= 0)
+        {
+
+            Health = MaxHealth;
+            
+        }
+
+        Health = health;
+        Debug.Log("Health - " + Health);
+
+    }
+
+}
